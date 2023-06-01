6904d0cc6c878ed20bb4de972e5405c755c28cc2
Gabriel Encarnacion <gencarnacion@talendig.com>
2023-06-01 17:32:29 -0400

    Cambios en log y portfolio services

--- services/LogService.js
+++ services/LogService.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service');
 const logSchema = require('../models/log');
-const utility = require('../utils');
-
+const { textResponseFormat } = require('../utils/utilsFunctions');
+const CustomAPIError = require('../errors/index');
+const { SHORTTEXTREPONSE } = require('../constants/helperConstants');
+const {statusUtils, Pagination} = require('../utils');
 /**
 * Create log
 * The creation of a new log.
@@ -12,7 +14,7 @@ const utility = require('../utils');
 * */
 const createLog = ({ log }) => {
 
-  const logData = {};
+  const logData = logSchema.create(log);
   const entityName = 'Log'
 
   if (!log) {
@@ -20,11 +22,11 @@ const createLog = ({ log }) => {
       textResponseFormat(entityName, SHORTTEXTREPONSE.notFound),
     );
   }
-    logData = logSchema.create(log);
+    
   return {
     payload: {
       hasError: false,
-      message: '',
+      message: 'Log creado',
       content: logData,
     },
   };
@@ -38,24 +40,25 @@ const createLog = ({ log }) => {
 * */
 const deleteLog = async ({ logId }) => {
 
-  const log = await utility.logUtils.getLogById(logId);
+  const logExists = await logSchema.findById(logId);
   const entityName = 'Log';
 
-  if (!log) {
+  if (!logExists) {
     throw new CustomAPIError.NotFoundError(
       textResponseFormat(entityName, SHORTTEXTREPONSE.notFound),
     );
   }
-  await utility.logUtils.deleteLogById(log._id);
+  {
+    const logDeleted = await logSchema.findByIdAndDelete(logId);
 
   return {
     payload: {
       hasError: false,
-      message: utility.utilsFunctions.textResponseFormat(entityName,SHORTTEXTREPONSE.deleted),
-      content: {},
+      message: textResponseFormat(entityName,SHORTTEXTREPONSE.deleted),
+      content: logDeleted,
     },
   };
-  
+};
 }; 
 /**
 * get logs
@@ -64,20 +67,31 @@ const deleteLog = async ({ logId }) => {
 * logPagination LogPagination Created log object
 * returns getLogs_200_response
 * */
-const getLogs = ({ logPagination }) => new Promise(
-  async (resolve, reject) => {
-    try {
-      resolve(Service.successResponse({
-        logPagination,
-      }));
-    } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
-    }
-  },
-);
+const getLogs = async ({ logPagination }) => {
+  const entityName = 'Log';
+
+  const { filter, pagination } = logPagination;
+
+  const paginationClass = new Pagination(pagination);
+  let queryPagination = paginationClass.queryPagination();
+
+  const logs = await logSchema.find(filter, null, queryPagination);
+  const count = await logSchema.countDocuments(filter);
+
+  queryPagination = { quantity: count, page: paginationClass.page };
+
+  return {
+    payload: {
+      hasError: false,
+      message: textResponseFormat(
+        entityName,
+        SHORTTEXTREPONSE.found,
+      ),
+      content: logs,
+      pagination: new Pagination(queryPagination),
+    },
+  };
+};
 /**
 * get log
 * get log.
@@ -85,20 +99,24 @@ const getLogs = ({ logPagination }) => new Promise(
 * logId String Id of the log
 * returns createUserContact_200_response
 * */
-const getSingleLog = ({ logId }) => new Promise(
-  async (resolve, reject) => {
-    try {
-      resolve(Service.successResponse({
-        logId,
-      }));
-    } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
-    }
-  },
-);
+const getSingleLog = async ({ logId }) => {
+
+  const LogData = await logSchema.findById(logId);
+  const entityName = 'Log';
+
+  if (!LogData) {
+    throw new CustomAPIError.NotFoundError(
+      textResponseFormat(entityName, SHORTTEXTREPONSE.notFound),
+    );
+  }
+  return {
+    payload: {
+      hasError: false,
+      message: '',
+      content: LogData,
+    },
+  };
+};
 /**
 * update log
 * update log.
@@ -107,21 +125,32 @@ const getSingleLog = ({ logId }) => new Promise(
 * contactCreated ContactCreated Created log object
 * returns createUserContact_200_response
 * */
-const updateLog = ({ logId, contactCreated }) => new Promise(
-  async (resolve, reject) => {
-    try {
-      resolve(Service.successResponse({
-        logId,
-        contactCreated,
-      }));
-    } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
-    }
-  },
-);
+const updateLog = async ({ logId, contactCreated }) => {
+
+  const entityName = 'Log';
+
+  if (logId !== contactCreated._id) {
+    throw new CustomAPIError.BadRequestError(SHORTTEXTREPONSE.errorId);
+  }
+
+  const { _id, ...values } = contactCreated;
+
+  const updated = await logSchema.updateOne({ _id }, values);
+
+  if (updated.modifiedCount !== 1) {
+    throw new Error(SHORTTEXTREPONSE.serverError);
+  }
+
+  const LogUpdated = await logSchema.findById(logId);
+
+  return {
+    payload: {
+      hasError: false,
+      message: textResponseFormat(entityName, SHORTTEXTREPONSE.updated),
+      content: LogUpdated,
+    },
+  };
+};
 
 module.exports = {
   createLog,
